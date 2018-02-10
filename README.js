f40d019d9f93137ab4b25622e2389e93d249f9f6
Ag mahan <36328949+Agmahan@users.noreply.github.com>
2018-02-10 17:07:26 +0530

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,13 @@
+custcodelink = {
+':party3:':'66740/1.gif',
+':notme2:':'46745/2.gif',
+':alright2:':'41060/3.gif',
+':notme3:':'37495/4.gif',
+':tomato:':'34157/5.gif',
+':girlmad:':'30828/6.gif',
+':girlclaps:':'28394/7.gif',
+':king:':'26361/8.gif',
+':laugh2:':'24040/9.gif',
+':cry3:':'26054/10.gif',
+':write:':'25062/11.gif'
+};
